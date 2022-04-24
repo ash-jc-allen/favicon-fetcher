bd6e0990e23b8a7a2d3e909264a381da2369217c
Ash Allen <mail@ashallendesign.co.uk>
2022-04-25 00:11:02 +0100

    Added fallback functionality.

--- src/Concerns/HasDefaultFunctionality.php
+++ src/Concerns/HasDefaultFunctionality.php
@@ -4,6 +4,7 @@ namespace AshAllenDesign\FaviconFetcher\Concerns;
 
 use AshAllenDesign\FaviconFetcher\Exceptions\FaviconNotFoundException;
 use AshAllenDesign\FaviconFetcher\Exceptions\InvalidArgumentException;
+use AshAllenDesign\FaviconFetcher\Facades\Favicon;
 use AshAllenDesign\FaviconFetcher\FetchedFavicon;
 
 trait HasDefaultFunctionality
@@ -17,9 +18,22 @@ trait HasDefaultFunctionality
         return $this->fetch($url) ?? $default($url);
     }
 
+    public function fetchOrFallback(string $url, array $drivers): ?FetchedFavicon
+    {
+        return $this->fetchOr($url, function (string $url) use ($drivers): ?FetchedFavicon {
+            foreach ($drivers as $driver) {
+                if ($favicon = Favicon::driver($driver)->fetch($url)) {
+                    return $favicon;
+                }
+            }
+
+            return null;
+        });
+    }
+
     public function fetchOrThrow(string $url): FetchedFavicon
     {
-        return $this->fetchOr($url, function ($url): void {
+        return $this->fetchOr($url, function (string $url): void {
             throw new FaviconNotFoundException('A favicon cannot be found for '.$url);
         });
     }
