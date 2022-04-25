ad35daa8c908854485b7d27edd91331bb591a566
Ash Allen <mail@ashallendesign.co.uk>
2022-04-25 23:49:07 +0100

    Added facade docblocks.

--- src/Facades/Favicon.php
+++ src/Facades/Favicon.php
@@ -2,9 +2,19 @@
 
 namespace AshAllenDesign\FaviconFetcher\Facades;
 
+use AshAllenDesign\FaviconFetcher\Contracts\Fetcher;
 use Illuminate\Support\Facades\Facade;
 use RuntimeException;
 
+/**
+ * @method static Fetcher driver(string $driver = null)
+ * @method static void extend(string $name, Fetcher $fetcher)
+ * @method static Fetcher throw(bool $throw = true)
+ * @method static Fetcher withFallback(string ...$fallbacks)
+ * @method static Fetcher useCache(bool $useCache = true)
+ * @method static Favicon|null fetch(string $url)
+ * @method static mixed fetchOr(string $url, mixed $default)
+ */
 class Favicon extends Facade
 {
     /**
