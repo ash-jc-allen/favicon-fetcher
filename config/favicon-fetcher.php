d9e031f0864afb98b5222b34733b8ab20af4bd67
Ash Allen <mail@ashallendesign.co.uk>
2022-04-26 00:25:10 +0100

    Add comments to config file.

--- config/favicon-fetcher.php
+++ config/favicon-fetcher.php
@@ -2,8 +2,30 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Default Driver
+    |--------------------------------------------------------------------------
+    |
+    | The driver that should be used by default for fetching the favicons.
+    | By default, the package comes with support for several drivers,
+    | but you can define your own if needed.
+    |
+    | Supported drivers: "http", "google-shared-stuff", "favicon-kit"
+    |
+    */
     'default' => 'http',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Caching
+    |--------------------------------------------------------------------------
+    |
+    | The package provides support for caching the fetched favicon's URLs.
+    | Here, you can specify the different options for caching, such as
+    | cache prefix that is prepended to all the cache keys.
+    |
+    */
     'cache' => [
         'prefix' => 'favicon-fetcher',
     ]
