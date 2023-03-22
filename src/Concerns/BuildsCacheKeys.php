134a6ea4c3d598bd8089d3424d1944c81e347d62
Ash Allen <mail@ashallendesign.co.uk>
2023-03-22 15:33:56 +0000

    WIP: Added the ability to cache a favicon collection.

--- src/Concerns/BuildsCacheKeys.php
+++ src/Concerns/BuildsCacheKeys.php
@@ -5,16 +5,26 @@ namespace AshAllenDesign\FaviconFetcher\Concerns;
 trait BuildsCacheKeys
 {
     /**
-     * Build the key used for caching the favicon's URL.
+     * Build the key used for caching the favicon.
      *
      * @param  string  $url
      * @return string
      */
-    protected function buildCacheKey(string $url): string
+    public function buildCacheKey(string $url): string
     {
-        // TODO Update this method to also use the type and and size of the favicon.
         $url = str_replace(['http://', 'https://'], '', $url);
 
         return config('favicon-fetcher.cache.prefix').'.'.$url;
+    }
+
+    /**
+     * Build the key used for caching the favicon collection.
+     *
+     * @param  string  $url
+     * @return string
+     */
+    public function buildCacheKeyForCollection(string $url): string
+    {
+        return $this->buildCacheKey($url).'.collection';
     }
 }
